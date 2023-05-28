8a36c84072240725ead1838453e7e8b24d88d435
Shalopay03 <134221829+Shalopay03@users.noreply.github.com>
2023-05-28 19:56:37 +0300

    add size changing option

--- scripts.js
+++ scripts.js
@@ -12,9 +12,20 @@ function fillPanel(panelSize){
     panel.appendChild(pixel);
 }
 
+function cleanPanel(){
+    const panel = document.querySelector('#painting-space');
+    const pixel = document.querySelector('.pixel');   
+    panel.removeChild(pixel);
+}
+
+function randomColor(){
+    return Math.floor(Math.random()*16777215).toString(16);
+}
+
 let chosenColor='red';
 let panelXSize = 10;
 let panelFullSize = panelXSize**2;
+let rainbowIsOn = false;
 
 for(let i=0; i<panelFullSize; i++) fillPanel(panelXSize);
 
@@ -23,6 +34,7 @@ const pixels = document.querySelectorAll('.pixel');
 pixels.forEach((pixel) => {
 
     pixel.addEventListener('mouseover', () => {
+        if (rainbowIsOn) chosenColor=`#${randomColor()}`;
         pixel.style.backgroundColor = chosenColor;        
     })
 
@@ -32,16 +44,39 @@ const color = document.querySelector('#color');
    
 color.addEventListener('input', () => {
     chosenColor=color.value;
+    rainbowIsOn=false;
 });
 
 const eraser = document.querySelector('#eraser');
 
 eraser.addEventListener('click', () =>{
     chosenColor='white';
+    rainbowIsOn=false;
 })
 
 const rainbow = document.querySelector('#rainbow');
 
-eraser.addEventListener('click', () =>{
-    chosenColor='red';
+rainbow.addEventListener('click', () =>{  
+    
+    rainbowIsOn=true;
 })
+
+//size
+const size = document.querySelector('#size');
+
+size.addEventListener('input', () => {
+    for(let i=0; i<panelFullSize; i++) cleanPanel();
+    panelXSize=size.value;
+    panelFullSize = panelXSize**2;
+    for(let i=0; i<panelFullSize; i++) fillPanel(panelXSize);
+
+    const pixels = document.querySelectorAll('.pixel');
+    pixels.forEach((pixel) => {
+
+        pixel.addEventListener('mouseover', () => {
+            if (rainbowIsOn) chosenColor=`#${randomColor()}`;
+            pixel.style.backgroundColor = chosenColor;        
+        })
+    
+    });
+});
